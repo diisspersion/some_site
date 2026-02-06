cddfdb40d13a389c332bf6b86efeefb723adfe0c
diisspersion <diisspersion>
2026-02-06 21:42:08 +0000

    CI test

--- app/app.py
+++ app/app.py
@@ -12,21 +12,25 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+
 class LoginRequest(BaseModel):
     username: str
     password: str
 
+
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html", context={"request": request})
+    return templates.TemplateResponse(request=request,
+                                      name="index.html",
+                                      context={"request": request})
+
 
 @app.post("/login")
 async def login(data: LoginRequest):
-    # Тут ваша логика проверки пароля
     if data.username == "admin" and data.password == "1234":
         return {"message": "Успешный вход", "token": "fake-jwt-token"}
-    
     return {"error": "Неверные данные"}, 400
+
 
 @app.get("/stats", response_class=HTMLResponse)
 async def stats_page(request: Request):
@@ -36,7 +40,5 @@ async def stats_page(request: Request):
         "disk_usage": psutil.disk_usage("/").percent,
         "processes": len(psutil.pids())
         }
-    
-    return templates.TemplateResponse("stats.html", {"request": request, **data})
-
-
+    return templates.TemplateResponse("stats.html",
+                                      {"request": request, **data})
